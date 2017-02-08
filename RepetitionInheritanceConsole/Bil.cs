add0d430a3f5c290a852543d17f62d53acbe7749
natsachaj <natascha_jakobsen@hotmail.com>
2017-02-08 09:54:27 +0100

    Add project files.

---
+++ RepetitionInheritanceConsole/Bil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionInheritanceConsole
+{
+    public class Bil
+    {
+        public int BilPrisExAfgift { get; set; }
+        public int KøbsÅr { get; set; }
+        public string Mærke { get; set; }
+        public string RegistreringsNr { get; set; }
+        public int KmPrLiter { get; set; }
+
+        public Bil()
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
